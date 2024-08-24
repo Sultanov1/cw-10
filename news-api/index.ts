a73b64f74f305d046b66a54d8c651ed27c3c7669
Oybek Sultanov <oibekzzz09@gmail.com>
2024-08-24 14:49:23 +0600

    Реализация cors данных на бд.

--- news-api/index.ts
+++ news-api/index.ts
@@ -1,9 +1,16 @@
-import express from "express";
+import express from 'express';
+import newsRouter from './routes/news';
+import cors from 'cors';
+import commentsRouter from './routes/comments';
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use(cors());
+
+app.use('/news', newsRouter);
+app.use('/comments', commentsRouter);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
